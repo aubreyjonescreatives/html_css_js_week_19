2c27e68e339110798b749629f99391f3916e242f
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-07-27 19:32:56 -0600

    added files and folders to week 19

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+
+
+function firstFunction(p1, p2) {
+
+var x = 4; 
+console.log(x); 
+
+return p1 * p2 + x; 
+console.log(p1, p2)
+
+}; 
+
+document.getElementById("demo").innerHTML = firstFunction(4, 3)
